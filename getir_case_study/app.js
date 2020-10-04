53e3b0b9e6b03a66eba3c0f28bcf2c96b54a9508
Çağdaş Başoğlu <cbasoglu@ebay.com>
2020-10-04 14:30:29 +0300

    Adds routing for record endpoint in app js

--- getir_case_study/app.js
+++ getir_case_study/app.js
@@ -4,10 +4,16 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const appConfig = require('./config/appConfig');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var recordsRouter = require('./routes/records');
 
 var app = express();
+
+// mongo connection set up
+appConfig.mongooseConnection();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +27,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+// routing for record endpoint
+app.use('/record', recordsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
